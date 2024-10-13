a924c46393f5bce68d42a98b53b2c143a398f649
Mba Uchenna <urchymanny@gmail.com>
2024-10-13 10:08:07 +0100

    feat(answer): implement Answer class to calculate scores
    Implemented the `Answer` Ruby class with methods to calculate the score based on the answer's parameters. The class processes inputs and returns the score, valid points, and percentage for the given answers.

---
+++ main.rb
@@ -0,0 +1,89 @@
+class Answer
+  attr_accessor :enjoys_job, :respected_by_managers, :good_for_carers,
+                :contracted_hours, :hours_actually_worked, :unpaid_extra_work,
+                :hourly_rate, :score
+
+  def initialize(enjoys_job:, respected_by_managers:, good_for_carers:,
+                 contracted_hours:, hours_actually_worked:, unpaid_extra_work:,
+                 hourly_rate:, max_overtime: 8, min_wage: 6.0, **_extras)
+
+    # Answer Parameters
+    @enjoys_job = enjoys_job
+    @respected_by_managers = respected_by_managers
+    @good_for_carers = good_for_carers
+    @contracted_hours = contracted_hours
+    @hours_actually_worked = hours_actually_worked
+    @unpaid_extra_work = unpaid_extra_work
+    @hourly_rate = parse_hourly_rate(hourly_rate) # serialize the input
+
+    # Counters
+    @score = 0
+    @valid_points = 0
+
+    # COY Fixed Values
+    @max_overtime = max_overtime
+    @min_wage = min_wage
+  end
+
+  def calculate_score
+    check_boolean_response(@enjoys_job)
+    check_boolean_response(@respected_by_managers)
+    check_boolean_response(@good_for_carers)
+    check_boolean_response(@unpaid_extra_work, positive_response: 'no')
+    check_hours
+    check_rate
+
+    percentage = (@score.to_f / @valid_points) * 100
+    [@score, @valid_points, percentage] # Return the scores
+  end
+
+  private
+
+  def parse_hourly_rate(rate)
+    return rate if rate.is_a?(Numeric)
+
+    rate.gsub('£', '').to_f
+  end
+
+  def check_boolean_response(value, positive_response: 'yes')
+    return if [nil, 'unsure'].include?(value)
+
+    @valid_points += 1
+    @score += 1 if value == positive_response
+  end
+
+  def check_hours
+    return if [nil, 'unsure'].include?(@hours_actually_worked) || [nil, 'unsure'].include?(@contracted_hours)
+
+    @valid_points += 1
+    overtime = @hours_actually_worked - @contracted_hours
+    @score += 1 if overtime <= @max_overtime
+  end
+
+  def check_rate
+    return if [nil, 'unsure'].include?(@hourly_rate)
+
+    @valid_points += 1
+    @score += 1 if @hourly_rate >= @min_wage
+  end
+end
+
+def runner(answer)
+  ans = Answer.new(**answer)
+  score, valid_points, percentage = ans.calculate_score
+  print("Score is #{score}/#{valid_points} - #{percentage.round(2)}")
+end
+
+sample = {
+  "enjoys_job": "yes",
+  "respected_by_managers": "no",
+  "good_for_carers": "yes",
+  "contracted_hours": 20,
+  "hours_actually_worked": 34,
+  "unpaid_extra_work": "unsure",
+  "age": 26,
+  "hourly_rate": "£8.22",
+  "submitted_date": 1608211454000
+}
+
+runner(sample) # outputs "Score is 3/5 - 60.0%"
